2bfe1c21450821341f88651ab43ffe0f42c85848
jabir <58764901+mjsidevops@users.noreply.github.com>
2023-12-08 20:27:15 +0530

    Update backend.tf

--- iac-infrastructure/parent-module/test-env/backend.tf
+++ iac-infrastructure/parent-module/test-env/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "dig-apitest-iac-state"
-    key            = "iac-node-assembler/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "dig-iac-state-lock"
+    bucket         = "test-bucket-name"
+    key            = "terraformstate/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "test-db-table"
   }
 }
